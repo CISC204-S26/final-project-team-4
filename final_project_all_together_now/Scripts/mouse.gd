7fcba062f085908e99729bacaf7d7568659f8551
rhodew <rhodew@udel.edu>
2026-04-29 16:12:39 -0400

    mode switch
    made the crosshair switch modes

--- final_project_all_together_now/Scripts/mouse.gd
+++ final_project_all_together_now/Scripts/mouse.gd
@@ -12,11 +12,31 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	follow_mouse()
+	if Input.is_action_just_pressed("scroll_up"):
+		swap_modes(1)
+	if Input.is_action_just_pressed("scroll_down"):
+		swap_modes(-1)
 
 
 func follow_mouse():
 	position = get_global_mouse_position()
 
 
-func swap_modes():
-	pass
+func swap_modes(mode):
+	current_mode = current_mode + mode
+	if current_mode > 3:
+		current_mode = 1
+	if current_mode < 1:
+		current_mode = 3
+	if current_mode == 1:
+		$Crosshair1.show()
+		$Crosshair2.hide()
+		$Crosshair3.hide()
+	if current_mode == 2:
+		$Crosshair1.hide()
+		$Crosshair2.show()
+		$Crosshair3.hide()
+	if current_mode == 3:
+		$Crosshair1.hide()
+		$Crosshair2.hide()
+		$Crosshair3.show()
