e2b3a79101238c038d25944e2463682f1c75d15b
LDilks <ldilks@udel.edu>
2026-05-06 14:18:51 -0400

    Grappling Hook WIP

--- final_project_all_together_now/Scripts/grappling_hook.gd
+++ final_project_all_together_now/Scripts/grappling_hook.gd
@@ -1,20 +1,28 @@
 extends Node2D
 
-var shoot_hook: bool = false;
+@onready var mouse = get_node("res://Scenes/mouse.tscn");
+var shoot_hook = false;
+var target = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#check moues input, if true
-		#shoot_hook = true;
+	if (Input.is_action_pressed("left_click")):
+		shoot_hook = true;
 	
 	if (shoot_hook):
-		_shoot_hook();
-		shoot_hook = false; #reset shoot_hook
-
-func _shoot_hook() -> void:
-	#code here for actually shooting the hook
-	pass
+		shoot_hook = false;
+		
+		var space_state = get_world_2d().direct_space_state;
+		var start = global_position;
+		var end = get_global_mouse_position();
+		
+		var raycast = PhysicsRayQueryParameters2D.create(start, end);
+		
+		var hook_target = space_state.intersect_ray(raycast);
+		
+		global_position = hook_target;
