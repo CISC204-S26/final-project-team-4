69e653b644d865595a6a4b0f7ebcda6223bc7973
Logan D Finney <ldfinn2107@gmail.com>
2026-05-06 12:58:42 -0400

    player movement

--- final_project_all_together_now/Scripts/player_keyboard.gd
+++ final_project_all_together_now/Scripts/player_keyboard.gd
@@ -3,9 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var cooldown = 0
 
 func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -21,5 +22,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
+	if Input.is_action_just_pressed("dash"):
+		if cooldown == 0:
+			cooldown += 50
+			if direction == 0:
+				velocity.x = 3000
+			else:
+				velocity.x = direction * SPEED * 40
+	if cooldown > 0:
+		cooldown -= 1
 	move_and_slide()
