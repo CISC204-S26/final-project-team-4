f1dfc554ed7bddc39867d41a549c42633fb4c5ff
rhodew <rhodew@udel.edu>
2026-05-13 14:08:37 -0400

    shield enemy
    coded the shield enemy

---
+++ final_project_all_together_now/Scripts/shielded_enemy.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+@export var health = 100
+@export var start_position = Vector2(0, 0)
+var shield = true
+var aggro = false
+var target
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if health < 1:
+		queue_free()
+	if shield == false:
+		if aggro == true:
+			var direction = global_position.direction_to(target.position)
+			position = position + direction
+		if position != start_position:
+			if aggro == false:
+				var direction = global_position.direction_to(start_position)
+				position = position + direction
+				if position.distance_to(start_position) < 10:
+					position = start_position
+					shield = true
+
+
+func damage(power):
+	if shield == false:
+		health = health - power
+
+
+func _on_detector_body_entered(body: Node2D) -> void:
+	shield = false
+	aggro = true
+	target = body
+
+
+func _on_detector_body_exited(body: Node2D) -> void:
+	aggro = false
